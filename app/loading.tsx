f2837b17eac02f8820e78f189c72c6c0964320df
Berthose Fin <berthosefin@gmail.com>
2023-10-24 19:15:42 +0300

    Update loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,16 @@
 import { Loader } from "@/components/ui/loader";
 
 export default function loading() {
-  return <Loader />;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Loader />
+    </div>
+  );
 }
